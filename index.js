6ff46a986a199b106866ae5ab882237b6a42c4e7
Dima Mironov <mattmars9548@gmail.com>
2024-03-18 20:10:20 +0300

    kkk

---
+++ index.js
@@ -0,0 +1,17 @@
+// let isShowPassword = false;
+// let email = '5151351';
+// const correctEmail = 'user.@mail.ru';
+// const inCorrectEmail = email;
+// let errormessage = null;
+
+
+
+// const validateEmail = (userEmail) => {
+//     let error = correctEmail === userEmail ? null : 'error';
+
+//   if (!!error) {
+//     alert(error);
+//   } else {
+//     confirm('good');
+//   }
+// }
